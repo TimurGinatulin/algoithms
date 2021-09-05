5661bb8e96f20e7c726f4c29e68a91fd448bc370
Tim <ginatulin.t@yandex.ru>
2021-09-05 16:49:18 +0500

    hw6

---
+++ algorithms/src/lesson6/Tree.java
@@ -0,0 +1,29 @@
+package lesson6;
+
+public interface Tree<E extends Comparable<? super E>> {
+
+    enum TraversMode {
+        IN_ORDER, PRE_ORDER, POST_ORDER
+    }
+    boolean checkBalanced();
+
+    int getSize();
+
+    int getMaxLevel();
+
+    int getLevel();
+
+    boolean add(E value);
+
+    boolean contains(E value);
+
+    boolean remove(E value);
+
+    boolean isEmpty();
+
+    int size();
+
+    void display();
+
+    void traverse(TraversMode mode);
+}
